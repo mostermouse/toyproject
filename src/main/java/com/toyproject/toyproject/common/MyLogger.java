a8787d52014e74f9fbc000f7ee783bd7274d8429
mostermouse <whdls7946@gmail.com>
2024-09-03 15:58:15 +0900

    스코프 지정 마무리

--- src/main/java/com/toyproject/toyproject/common/MyLogger.java
+++ src/main/java/com/toyproject/toyproject/common/MyLogger.java
@@ -10,6 +10,10 @@ import java.util.UUID;
 
 @Component
 @Scope(value = "request" , proxyMode = ScopedProxyMode.TARGET_CLASS)
+/*[bd86dd56-3121-4769-94db-a9022f4bef92] request scope bean create: com.toyproject.toyproject.common.MyLogger@5dfba55d
+[bd86dd56-3121-4769-94db-a9022f4bef92 http://localhost:8080/log-demo] controller test
+[bd86dd56-3121-4769-94db-a9022f4bef92 http://localhost:8080/log-demo] service id =testId
+[bd86dd56-3121-4769-94db-a9022f4bef92] request scope bean close: com.toyproject.toyproject.common.MyLogger@5dfba55d*/
 //싱글톤 처럼 동작하는거처럼 보이기에 다른 사람이 오해 할 수도 있으니 협의 하에 사용 해야 함
 public class MyLogger {
     private String uuid;
--- src/main/java/com/toyproject/toyproject/web/LogDemoService.java
+++ src/main/java/com/toyproject/toyproject/web/LogDemoService.java
@@ -3,14 +3,23 @@ package com.toyproject.toyproject.web;
 import com.toyproject.toyproject.common.MyLogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+//생성자 자동 생성해서 싱글톤 패턴 으로 해줌
 public class LogDemoService {
     //private final ObjectProvider<MyLogger> myLoggerProvider;
+
     private final MyLogger myLogger;
+
+    /*@Autowired @RequiredArgsConstructor얘가 대신 만들어 줌
+    public LogDemoService(MyLogger myLogger) {
+        this.myLogger = myLogger;
+    }*/
+
     public void logic(String id) {
       //  MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id =" + id);
